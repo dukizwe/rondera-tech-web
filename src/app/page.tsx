bd374547c727a61311a1e8f159a5c218cc85078c
dukizwe <mbagapro@gmail.com>
2024-11-01 19:15:42 +0200

    redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import { useEffect } from "react"
 export default function Home() {
    useEffect(() => {
-      // window.location.href = "https://play.google.com/store/apps/details?id=com.dukizwe.betteur"
+      window.location.href = "https://play.google.com/store/apps/details?id=com.dukizwe.betteur"
    }, [])
    return null
 }
