82a6a7fef1d6fb43df8f08575b5d14c775753b4f
dukizwe <mbagapro@gmail.com>
2024-12-04 13:47:49 +0200

    idk

--- src/app/discussions/d/[discussionId]/[slug]/page.tsx
+++ src/app/discussions/d/[discussionId]/[slug]/page.tsx
@@ -2,22 +2,22 @@ import { decodeId } from "@/helpers/IdEncryption";
 import { Metadata } from "next";
 import { notFound, redirect } from "next/navigation";
 
-const baseUrl = true ? "https://rondera-test.onrender.com" : "http://192.168.217.153:3000"
+const baseUrl = false ? "https://rondera-test.onrender.com" : "http://192.168.222.153:3000"
 type Params = {
    params: Promise<{
-      opportunityId: string,
+      discussionId: string,
       slug: string
    }>
 }
 
 export async function generateMetadata({ params }: Params): Promise<Metadata> {
-   const encodedOpportunityId = (await params).opportunityId
+   const encodedOpportunityId = (await params).discussionId
    const opportunityId = decodeId(encodedOpportunityId)
-   const opportunityRes = await fetch(`${baseUrl}/opportunities/${opportunityId}`)
+   const opportunityRes = await fetch(`${baseUrl}/discussions/${opportunityId}`)
    const opportunity = (await opportunityRes.json()).data
    if (!opportunity) {
       return {
-         title: "Opportunity not found",
+         title: "Discussion not found",
       }
    }
    const postPhotourl = opportunity.photo || opportunity.linkMetadata?.image
@@ -36,16 +36,14 @@ export async function generateMetadata({ params }: Params): Promise<Metadata> {
    }
 }
 
-export default async function OpportunityDetailsPage({ params }: Params) {
-   const encodedOpportunityId = (await params).opportunityId
-   const opportunityId = decodeId(encodedOpportunityId)
-   const opportunityRes = await fetch(`${baseUrl}/opportunities/${opportunityId}`)
+export default async function DiscussionDetailsPage({ params }: Params) {
+   const encodedDiscussionId = (await params).discussionId
+   const discussionId = decodeId(encodedDiscussionId)
+   const opportunityRes = await fetch(`${baseUrl}/discussions/${discussionId}`)
    const opportunity = (await opportunityRes.json()).data
    if (!opportunity) {
       return notFound()
    }
    redirect("https://play.google.com/store/apps/details?id=com.dukizwe.betteur")
-   // const postPhotourl = opportunity.photo || opportunity.linkMetadata?.image
-   // window.location.href = `betteur://rondera${window.location.pathname}`
    return null
 }
